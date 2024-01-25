9d62a3b6158a51a17874e187939f3295a281684c
tigerjoson <32243046+tigerjoson@users.noreply.github.com>
2024-01-25 22:31:58 +0800

    nodejsdemop1
    import and export

---
+++ index.js
@@ -0,0 +1,5 @@
+const mytool = require("./mymodule");
+const {look_keys ,print} = mytool
+const  A ="hi"
+
+mytool.print(A)
---
+++ mymodule.js
@@ -0,0 +1,12 @@
+function look_keys(object){
+	console.log(Object.keys(object));
+}
+
+function print(object){
+	console.log(object);
+}
+
+module.exports ={
+	look_keys ,
+	print ,
+};
